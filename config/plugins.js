f6abb612866fc69a724d3894414ccc84e5e7bb74
12BSG12 <carlj8335@gmail.com>
2023-07-04 19:07:26 +0500

    init git commit -m init git commit -m init

---
+++ config/plugins.js
@@ -0,0 +1,32 @@
+module.exports = ({ env }) => ({
+  ckeditor: {
+    enabled: true,
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: 'strapi-provider-email-resend',
+      providerOptions: {
+        apiKey: env('RESEND_API_KEY'), // Required
+      },
+      settings: {
+        defaultFrom: 'b5g12@yandex.ru',
+        defaultReplyTo: 'b5g12@yandex.ru',
+      },
+    }
+  }, 
+});
